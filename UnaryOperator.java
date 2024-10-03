16bde6b7630758f7fc2673323014e8625d18af32
Geetika <139126957+Geetika2708@users.noreply.github.com>
2024-10-03 11:07:41 +0530

    Create UnaryOperator.java

---
+++ UnaryOperator.java
@@ -0,0 +1,12 @@
+// unary operator 
+
+public class UnaryOperator{
+  public static void main(String[]args){
+    int num_1 = 4;
+    int num_2 = 5;
+    System.out.println("increment is : " + (++num_1));
+    System.out.println(num_1);
+    System.out.println("decrement is : " + (--num_1));
+    System.out.println(num_1);
+  }
+}
