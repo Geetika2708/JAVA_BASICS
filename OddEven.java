d77b1996652b7afe692a5e7f061100e7c3b89432
Geetika <139126957+Geetika2708@users.noreply.github.com>
2024-10-03 13:16:56 +0530

    Create OddEven.java

---
+++ OddEven.java
@@ -0,0 +1,18 @@
+// odd even 
+
+import java.util.Scanner;
+
+public class OddEven{
+  public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter the number : ");
+    int num = sc.nextInt();
+    if(num%2 == 0 ){
+      System.out.println(num + "  is even number.");
+    }
+    else {
+      System.out.println(num + "  is odd number.");
+    }
+
+  }
+}
