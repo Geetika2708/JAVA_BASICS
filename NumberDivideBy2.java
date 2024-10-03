3bf6dfcd6d542876dad74abb2144def5d4715f5f
Geetika <139126957+Geetika2708@users.noreply.github.com>
2024-10-03 13:06:35 +0530

    Create NumberDivideBy2.java

---
+++ NumberDivideBy2.java
@@ -0,0 +1,13 @@
+// number divide by 2 
+
+import java.util.Scanner;
+public class NumberDivideBy2{
+  public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter the number : ");
+    int num = sc.nextInt();
+    if(num%2 == 0 ){
+System.out.println(num + "is divisible by 2");
+    }
+  }
+}
